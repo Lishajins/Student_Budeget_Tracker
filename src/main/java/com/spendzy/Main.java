c3d9cedb3f61a70fb3b40f9eb51330f14b666b5e
Pavithra G Nair <pavithragnair2005@gmail.com>
2025-09-06 07:36:21 +0530

    Main.java

--- src/main/java/com/spendzy/Main.java
+++ src/main/java/com/spendzy/Main.java
@@ -1,1 +1,10 @@
+package com.spendzy.ui;
+
+import com.spendzy.ui.frames.DashboardFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        new DashboardFrame();
+    }
+}
 
